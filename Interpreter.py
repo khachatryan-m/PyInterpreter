e5b2143f40e4f07ab1755a78a5dbac01f41077cd
Melanya <khachatryanmelanya7@gmail.com>
2026-01-20 00:48:41 +0400

    added more functionalities and made the code reusable

--- Interpreter.py
+++ Interpreter.py
@@ -1,6 +1,28 @@
 class Interpreter:
     def __init__(self):
         self.stack = []   # representing the stack as a list, stack is needed to load values & instructions there
+        self.environment = {}
+
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.environment[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.environment[name]
+        self.stack.append(val)
+
+    def parse_argument(self, instruction, argument, what_to_exec):
+        """ Understand what the argument to each instruction means."""
+        numbers = ["LOAD_VALUE"]
+        names = ["LOAD_NAME", "STORE_NAME"]
+
+        if instruction in numbers:
+            argument = what_to_exec["values"][argument]
+
+        elif instruction in names:
+            argument = what_to_exec["names"][argument]
+
+        return argument
 
 
     def LOAD_VALUE(self, value):
@@ -18,13 +40,13 @@ class Interpreter:
 
     def RUN_CODE(self, what_to_execute):
         instructions = what_to_execute["instructions"]
-        values = what_to_execute["values"]
 
         for i in instructions:
             instruction, arg = i
+            arg = self.parse_argument(instruction, arg, what_to_execute)
+
             if instruction == "LOAD_VALUE":
-                value = values[arg]  # get the number and load it
-                self.LOAD_VALUE(value)
+                self.LOAD_VALUE(arg)
 
             elif instruction == "ADD_TWO_VALUES":
                 self.ADD_TWO_VALUES()
@@ -32,14 +54,58 @@ class Interpreter:
             elif instruction == "PRINT_ANSWER":
                 self.PRINT_ANSWER()
 
+            elif instruction == "STORE_NAME":
+                self.STORE_NAME(arg)
+
+            elif instruction == "LOAD_NAME":
+                self.LOAD_NAME(arg)
+
+    def execute(self, what_to_execute):  # better way of the above func
+        instructions = what_to_execute["instructions"]
+
+        for i in instructions:
+            instruction, arg = i
+            arg = self.parse_argument(instruction, arg, what_to_execute)
+            ## Give me the method of this object (interpreter) whose name is stored in the string instruction
+            bytecode_method = getattr(self, instruction)  # get instruct. replacement for the if-else for each instruct
+            if arg is None:
+                bytecode_method()  # some methods don't take args, e.g. ADD-TWO_VALUES
+            else:
+                bytecode_method(arg)
+
+
+
+
 
 ###### TESTING
-what_to_execute = {
+what_to_execute2 = {
     "instructions": [("LOAD_VALUE", 0),  # the first number
                      ("LOAD_VALUE", 1),  # the second number
                      ("ADD_TWO_VALUES", None),
                      ("PRINT_ANSWER", None)],
     "values": [7, 5] }
 
+what_to_execute3 = {
+        "instructions": [("LOAD_VALUE", 0),
+                         ("LOAD_VALUE", 1),
+                         ("ADD_TWO_VALUES", None),
+                         ("LOAD_VALUE", 2),
+                         ("ADD_TWO_VALUES", None),
+                         ("PRINT_ANSWER", None)],
+        "values": [7, 5, 8] }
+
+what_to_execute4 = {
+        "instructions": [("LOAD_VALUE", 0),
+                         ("STORE_NAME", 0),
+                         ("LOAD_VALUE", 1),
+                         ("STORE_NAME", 1),
+                         ("LOAD_NAME", 0),
+                         ("LOAD_NAME", 1),
+                         ("ADD_TWO_VALUES", None),
+                         ("PRINT_ANSWER", None)],
+        "values": [1, 2],
+        "names":   ["a", "b"] }
+
+
 interpreter = Interpreter()
-interpreter.RUN_CODE(what_to_execute)
+interpreter.RUN_CODE(what_to_execute4)
